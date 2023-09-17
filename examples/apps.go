af093547d51ce8173562bb12b72e90eaeb19d9c5
Mohsen Mirzakhani <mohsen.mkh88@gmail.com>
2023-09-17 21:27:41 +0200

    retry and timeout

--- examples/apps.go
+++ examples/apps.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"errors"
 	"log"
+	"time"
 
 	"github.com/mirzakhany/sysd"
 )
@@ -10,7 +12,7 @@ import (
 type appA struct {
 }
 
-func (a *appA) Start(ctx context.Context) error {
+func (a *appA) Start(ctx context.Context, restored bool) error {
 	log.Println("appA started")
 
 	defer func() {
@@ -18,17 +20,14 @@ func (a *appA) Start(ctx context.Context) error {
 	}()
 
 	return sysd.ShutdownGracefully(ctx, func() error {
+		time.Sleep(5 * time.Second)
 		log.Println("appA received shutdown signal")
 		return nil
 	})
 }
 
-func (a *appA) Stop(ctx context.Context) error {
-	//TODO implement me
-	panic("implement me")
-}
-
 func (a *appA) Status(ctx context.Context) error {
+	log.Println("appA status")
 	return nil
 }
 
@@ -37,15 +36,14 @@ func (a *appA) Name() string {
 }
 
 type appB struct {
+	failCounter int
 }
 
-func (a *appB) Stop(ctx context.Context) error {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (a *appB) Start(ctx context.Context) error {
+func (a *appB) Start(ctx context.Context, restored bool) error {
 	log.Println("appB started")
+
+	// rest failure counter
+	a.failCounter = 0
 
 	defer func() {
 		log.Println("appB stopped")
@@ -58,9 +56,56 @@ func (a *appB) Start(ctx context.Context) error {
 }
 
 func (a *appB) Status(ctx context.Context) error {
+	log.Println("appB status")
+	a.failCounter++
+	if a.failCounter > 3 {
+		return errors.New("appB failed")
+	}
 	return nil
 }
 
 func (a *appB) Name() string {
 	return "appB"
+}
+
+type appC struct {
+	sucessCounter  int
+	restoreCounter int
+}
+
+func (a *appC) Start(ctx context.Context, restored bool) error {
+	log.Println("appC started")
+
+	if restored {
+		a.restoreCounter++
+	}
+
+	if a.restoreCounter > 2 {
+		// lets hard fail the app after 2 retries
+		return errors.New("appC failed to restore")
+	}
+
+	defer func() {
+		log.Println("appC stopped")
+	}()
+
+	return sysd.ShutdownGracefully(ctx, func() error {
+		log.Println("appC received shutdown signal")
+		return nil
+	})
+}
+
+func (a *appC) Status(ctx context.Context) error {
+	log.Println("appC status")
+	a.sucessCounter++
+
+	// fail until the app is restored
+	if a.sucessCounter > 2 && a.restoreCounter <= 2 {
+		return errors.New("appC failed")
+	}
+	return nil
+}
+
+func (a *appC) Name() string {
+	return "appC"
 }
--- examples/simple.go
+++ examples/simple.go
@@ -2,18 +2,31 @@ package main
 
 import (
 	"syscall"
+	"time"
 
 	"github.com/mirzakhany/sysd"
 )
 
 func main() {
 	systemd := sysd.NewSystemd()
+	systemd.SetGraceFulShutdownTimeout(4 * time.Second)
+	systemd.SetStatusCheckInterval(1 * time.Second)
+
 	if err := systemd.Add(&appA{}); err != nil {
 		panic(err)
 	}
-	if err := systemd.Add(&appB{}); err != nil {
+
+	appB := &appB{}
+	if err := systemd.Add(appB); err != nil {
 		panic(err)
 	}
+	systemd.SetAppOnFailure(appB.Name(), sysd.OnFailureIgnore)
+
+	appC := &appC{}
+	if err := systemd.Add(appC); err != nil {
+		panic(err)
+	}
+	systemd.SetAppOnFailure(appC.Name(), sysd.OnFailureRestart.Retry(4).RetryTimeout(2*time.Second))
 
 	// listen for os exit signals
 	ctx := sysd.ContextWithSignal(syscall.SIGINT, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGQUIT)
